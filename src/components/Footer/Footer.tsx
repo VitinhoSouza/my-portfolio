1e6ad761d8379ec5bed416c1ffd4343af0b6264d
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2023-03-28 00:08:44 -0300

    feat: add target blank on social media links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,24 +12,35 @@ export function Footer() {
     <Container>
       <Copyright>Copyright © 2022 VitinhoDev. All Rights Reserved</Copyright>
       <SocialNetworks>
-        <a href="https://github.com/VitinhoSouza" style={{ color: "white" }}>
+        <a
+          href="https://github.com/VitinhoSouza"
+          style={{ color: "white" }}
+          target="_blank"
+          rel="noreferrer"
+        >
           <GithubLogo size="2rem" alt="Github" />
         </a>
         <a
           href="https://www.linkedin.com/in/victor-souza-1419b21ab/"
           style={{ color: "white" }}
+          target="_blank"
+          rel="noreferrer"
         >
           <LinkedinLogo size="2rem" alt="Linkedin" />
         </a>
         <a
           href="https://www.instagram.com/vitiinho_souzza/"
           style={{ color: "white" }}
+          target="_blank"
+          rel="noreferrer"
         >
           <InstagramLogo size="2rem" alt="Instagram" />
         </a>
         <a
           href="mailto:vitinhosouzajuatama@gmail.com"
           style={{ color: "white" }}
+          target="_blank"
+          rel="noreferrer"
         >
           <GoogleLogo size="2rem" alt="Gmail" />
         </a>
