396d3d1a05e4d4bc02438be05a5651711f1b5688
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2024-10-30 16:05:37 -0300

    feat: updating base color of light theme to blue

--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -2,14 +2,14 @@
 export default {
     title: 'light',
 
-    colors:{
-        primary:'#951ebb',
-        background:'#dfc5e8',
-        background_card:'#bd88d0',
+    colors: {
+        primary: '#1414b8',
+        background: '#b1b1df',
+        background_card: '#6f6fd1',
 
-        
+
         text_primary: '#FFF',
-        text_background:'#333',
-        text_card:'#333'
+        text_background: '#333',
+        text_card: '#333'
     }
 }
